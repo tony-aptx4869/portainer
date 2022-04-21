32b8989d18caddd1746230a372d2080efb8930d6
Tony Chang <tonychang7869@gmail.com>
2022-04-21 14:04:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+利用 docker-compose 部署 Portainer
+Deploy Portainer Using docker-compose
+=======================================
+
+快速利用 docker-compose 来部署 Portainer。
+
+Quickly deploy Portainer using docker-compose.
+
+个人很喜欢使用 Portainer 这个工具。
+
+I personally do love to use Portainer as a tool.
---
+++ ubuntu-install-docker-ce.sh
@@ -0,0 +1,16 @@
+
+apt remove docker docker.io containerd runc docker-engine
+
+apt install ca-certificates curl gnupg lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+dpkg --print-architecture
+
+lsb_release -cs
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update
+
+apt install docker-ce docker-ce-cli containerd.io
